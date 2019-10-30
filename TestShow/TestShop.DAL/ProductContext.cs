ff4013ac7cc6cc920b5cce0243d32ef49dadfd1b
Black Reaver <losha.pisnyak42@gmail.com>
2019-10-30 21:01:24 +0200

    Created db model

---
+++ TestShow/TestShop.DAL/ProductContext.cs
@@ -0,0 +1,17 @@
+namespace TestShop.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ProductContext : DbContext
+    {
+        public ProductContext()
+            : base("name=ProductContext")
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+
+   
+}
